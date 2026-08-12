97d8409483d370a54f0a06a64a2fd7c30e03e3b7
Nate <nate.bass@outlook.com>
2026-08-11 21:18:34 -0700

    Clean up PowerShell.
    * Remove templates.

--- powershell/templates/other/js/f.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export interface fProps {
-  children: React.ReactNode;
-}
-export default function f({ children }: fProps) {
-  return <div className="">{children}</div>;
-}
--- powershell/templates/vite/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-})
