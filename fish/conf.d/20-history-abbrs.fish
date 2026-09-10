dbb9824fb76587289dd7b2ae3cdee356d3f079cc
Nate <nate.bass@outlook.com>
2026-09-09 19:35:04 -0700

    Refactor better history abbreviations.

--- fish/conf.d/20-history-abbrs.fish
+++ fish/conf.d/20-history-abbrs.fish
@@ -1,16 +1,2 @@
-# History expansion abbreviations
-function _last_history_item
-    echo $history[2]
-end
-
-function _second_to_last_history_item
-    echo $history[3]
-end
-
-function _third_to_last_history_item
-    echo $history[4]
-end
-
-abbr -a !! --position anywhere --function _last_history_item
-abbr -a 2  --position anywhere --function _second_to_last_history_item
-abbr -a 3  --position anywhere --function _third_to_last_history_item
+# History expansion abbreviations (defined in functions/better_history.fish)
+better_history
