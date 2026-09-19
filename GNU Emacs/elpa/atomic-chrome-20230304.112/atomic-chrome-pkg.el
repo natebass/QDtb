e4bfcba6a474c0f24ebf1f264962ef91fb5a000d
Nate <nate.bass@outlook.com>
2026-09-19 04:18:40 -0700

    Add a configuration structure for Emacs 31 .

---
+++ GNU Emacs/elpa/atomic-chrome-20230304.112/atomic-chrome-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "atomic-chrome" "20230304.112"
+  "Edit Chrome text area with Emacs using Atomic Chrome."
+  '((emacs     "24.4")
+    (let-alist "1.0.4")
+    (websocket "1.4"))
+  :url "https://github.com/alpha22jp/atomic-chrome"
+  :commit "f1b077be7e414f457191d72dcf5eedb4371f9309"
+  :revdesc "f1b077be7e41"
+  :keywords '("chrome" "edit" "textarea")
+  :authors '(("alpha22jp" . "alpha22jp@gmail.com"))
+  :maintainers '(("alpha22jp" . "alpha22jp@gmail.com")))
